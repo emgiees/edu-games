597df5a8344cf2e9e18e06778174174f4af9db4c
emgiees <michalsuchanek@gmail.com>
2026-01-15 11:05:08 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -10,9 +10,10 @@
 //
 // Then upload this entire folder to GitHub Pages!
 
-const firebaseConfig = {
+window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyCSCc7QlbUv8f9Iae8eRZtLo9ER25feXkU",
   authDomain: "my-edu-games.firebaseapp.com",
+  databaseURL: "https://my-edu-games-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "my-edu-games",
   storageBucket: "my-edu-games.firebasestorage.app",
   messagingSenderId: "535387410502",
